e78b3eed14db97fbaf83a7e7fd389c50138e1234
Warun Sharma <warunsharma@Mac.lan>
2025-04-22 22:31:17 +0530

    feat: cpu intensive task

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ const express = require('express');
 const PORT = process.env.PORT || 4001;
 const app = express();
 
+app.get('/blocking', (req, res) => {
+    let counter = 0;
+    for (let i = 1; i < 20000000000; ++i) {
+        counter++;
+    }
+    res.send(`Counter is ${counter}`);
+})
+
+app.get('/non-blocking', (req, res) => {
+    res.send(`Non blocking request`);
+})
+
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
 })
