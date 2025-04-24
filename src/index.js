1a217fe5785a66b6fe65e8174bc2f3b369a0fb98
Warun Sharma <warunsharma@Mac.lan>
2025-04-24 18:30:47 +0530

    feat: clustering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-const express = require('express');
+import express from 'express';
 
 const PORT = process.env.PORT || 4001;
 const app = express();
+
+console.log(`worker pid=${process.pid}`);
+
+app.get('/heavy', (req, res) => {
+    let counter = 0;
+
+    for (let i = 0; i < 5000000; ++i) {
+        counter++;
+    }
+
+    res.send(`The result of cpu intensive task is ${counter}`);
+})
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
