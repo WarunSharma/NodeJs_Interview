c92f4e9fdf99ca94a0a25e0f22c9b48a28ead6ff
Warun Sharma <warunsharma@Mac.lan>
2025-04-27 06:24:08 +0530

    feat: transform stream

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const {Transform, pipeline} = require('stream');
 const fs = require('fs');
+const { error } = require('console');
 
 const PORT = process.env.PORT || 4001;
 const app = express();
@@ -11,10 +13,29 @@ const app = express();
 //     console.log("New chuck: ", chunck.toString());
 // })
 
-const filePath = path.join(__dirname, 'my-file-write.txt');
-const writable = fs.createWriteStream(filePath);
-writable.write("Hello World");
-writable.end("!");
+// const filePath = path.join(__dirname, 'my-file-write.txt');
+// const writable = fs.createWriteStream(filePath);
+// writable.write("Hello World");
+// writable.end("!");
+
+const readFilePath = path.join(__dirname, 'my-file-read.txt');
+const writeFilePath = path.join(__dirname, 'my-file-write.txt');
+
+const readable = fs.createReadStream(readFilePath, {highWaterMark: 20});
+const writable = fs.createWriteStream(writeFilePath);
+
+const uppercase = new Transform({
+    transform(chunck, encoding, callback) {
+        callback(null, chunck.toString().toUpperCase());
+    }
+})
+
+// readable.pipe(uppercase).pipe(writable);
+pipeline(readable, uppercase, writable, (error) => {
+    if (error)
+        console.log(error);
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
