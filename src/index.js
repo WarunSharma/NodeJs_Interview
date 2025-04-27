c359819dd57d4ca1facf8a3d74e353e48e67aa55
Warun Sharma <warunsharma@Mac.lan>
2025-04-27 06:06:45 +0530

    feat: read stream

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 4001;
 const app = express();
+const filePath = path.join(__dirname, 'my-file-read.txt');
+const readable = fs.createReadStream(filePath, {highWaterMark: 20})
+
+readable.on("data", (chunck) => {
+    console.log("New chuck: ", chunck.toString());
+})
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
