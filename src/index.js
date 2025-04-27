3e625f3ab765e604a555e2661ac215c027029678
Warun Sharma <warunsharma@Mac.lan>
2025-04-28 05:24:48 +0530

    feat: event loop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = process.env.PORT || 4001;
 const app = express();
 
+console.log("🔵 Start of the script (sync)");
+
+process.nextTick(() => {
+  console.log("🟢 Inside process.nextTick");
+});
+
+Promise.resolve()
+  .then(() => {
+    console.log("🟡 Inside Promise.then 1");
+    return Promise.resolve();
+  })
+  .then(() => {
+    console.log("🟡 Inside Promise.then 2");
+  });
+
+async function asyncFunction() {
+  console.log("🟣 Inside asyncFunction (start)");
+  await Promise.resolve();
+  console.log("🟣 Inside asyncFunction (after await)");
+}
+asyncFunction();
+
+setTimeout(() => {
+  console.log("🟠 Inside setTimeout 0ms");
+}, 0);
+
+setInterval(() => {
+  console.log("🔴 Inside setInterval 0ms");
+}, 0);
+
+setImmediate(() => {
+  console.log("🟤 Inside setImmediate");
+});
+
+console.log("🔵 End of the script (sync)");
+
 app.listen(PORT, () => {
-    console.log(`Server started at ${PORT}`);
-})
+  console.log(`Server started at ${PORT}`);
+});
